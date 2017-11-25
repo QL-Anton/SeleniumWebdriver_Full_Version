b6779374bab8e219774b6f8384e3f419bfbd5908
Anton-Kiselev <antohakiselev@yandex.ru>
2017-11-25 14:59:56 +0300

    Подготовлена инфраструктура
    Добавлен также пробный тест

---
+++ litecart/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -0,0 +1,42 @@
+package ru.stqa.training.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+/**
+ * Created by Антон on 25.11.2017.
+ */
+public class MyFirstTest {
+
+  private WebDriver driver;
+  private WebDriverWait wait;
+
+
+  @BeforeMethod
+  public void start() {
+    driver = new ChromeDriver();
+    wait = new WebDriverWait(driver, 10);
+  }
+
+  @Test
+  public void myFirstTest() {
+    driver.get("http://www.google.com/");
+
+
+
+  }
+
+
+  @AfterMethod
+  public void stop() {
+    driver.quit();
+    driver = null;
+
+  }
+}
